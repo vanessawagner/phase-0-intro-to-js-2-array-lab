6fda756fddb3139c07e762678f298f8eab8ea67a
Vanessa Wagner <vmwagner@gmail.com>
2022-05-18 18:27:11 -0500

    commit tree 17718a779f2ae828dbad72e2348ed2e4ec8660f0

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+cats;
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+    return cats
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+    return cats
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+    return cats;
+}
+
+function appendCat() {
+    appendCat = cats.slice();
+    appendCat.push("Broom")
+    return appendCat
+}
+
+function prependCat() {
+    prependCat = cats.slice();
+    prependCat.unshift("Arnold")
+    return prependCat
+}
+
+function removeLastCat() {
+    removeLastCat = cats.slice();
+    removeLastCat.pop();
+    return removeLastCat
+}
+
+function removeFirstCat() {
+    removeFirstCat = cats.slice();
+    removeFirstCat.shift();
+    return removeFirstCat
+}
